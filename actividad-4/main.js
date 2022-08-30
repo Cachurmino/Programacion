ea2c89ef379d5cd0fa0884553eeb87ab223d9890
Cachurmino <nachocappellini@gmail.com>
2022-08-30 17:04:32 -0300

    Repaso de Arreglos

--- actividad-4/main.js
+++ actividad-4/main.js
@@ -1,11 +1,17 @@
 import Catalogo from "./catalogo.js";
 
 const obtener_api = async () => {
-  const productos = await fetch(`https://fakestoreapi.com/products`);
+  const productos = await fetch(
+    `https://fakestoreapi.com/products${
+      document.getElementById("slt_categoria").value
+    }`
+  );
   const lista_productos = await productos.json();
 
   let catalogo = new Catalogo(lista_productos);
   catalogo.mostrar_cards();
 };
 
-obtener_api();
+document
+  .getElementById("slt_categoria")
+  .addEventListener("change", obtener_api);
---
+++ repaso_js/arreglos/main.js
@@ -0,0 +1,20 @@
+//Creamos un arreglo
+let lenguajes = ["Java", "Python", "JavaScrip", "C++", "Visual Basic", "Go"];
+
+//Longitud de un arreglo
+lenguajes.length;
+
+//Acceder al ultimo elemento de un arreglo
+lenguajes[lenguajes.length - 1];
+
+//agregar un elemento al final del arreglo
+lenguajes.push("Php");
+
+//Eliminar el ultimo elemento
+lenguajes.pop();
+
+//Agregar elemento al comienzo
+lenguajes.unshift("C#");
+
+//Eliminar elemento al comienzo
+lenguajes.shift();
